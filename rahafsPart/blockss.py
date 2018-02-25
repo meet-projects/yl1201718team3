b4432fb175a1050d545b10822593675ee6a6cf05
rahaf sabri <rahaf.sabri@gmail.com>
2018-02-25 17:25:22 +0200

    you

--- rahafsPart/blockss.py
+++ rahafsPart/blockss.py
@@ -4,70 +4,103 @@ import random
 import math
 import time
 turtle.colormode(255)
-
-me=6
-blocks_num=5
+turtle.hideturtle()
+turtle.pu()
+turtle.goto(0,300)
+turtle.tracer(0)
+me=8
+SLEEP=0.0065
+me2=1
+blocks_num=6
+RUNNING=True
+SCREEN_WIDTH=400
+SCREEN_HEIGHT=400
 block_list=[]
-xposes=[-90,-50,-10,10,30,50,90]
+xposes=[-400,-300,-200,-100,0,100,200,300,400,500]
 class BLOCK(Turtle):
 	def __init__(self,x,y,number):
 		Turtle.__init__(self)
+		self.hideturtle()
 		self.shape("square")
 		self.pu()
 		self.x=x
 		self.y=y
-		self.dy=5
+		self.goto(x,y)
+		self.dy=15
 		self.height=2
-		self.shapesize(2)
+		self.shapesize(4)
 		r=random.randint(0,225)
 		g=random.randint(0,225)
 		b=random.randint(0,225)
 		color=(r,g,b)
 		self.color(color)
-	def move(self,screen_width,screen_height):
+		self.showturtle()
+	def move(self):
 		current_x=self.xcor()
 		current_y=self.ycor()
 		new_y=current_y-self.dy
 		new_x=current_x
 		buttom_side_ball=new_y-self.height
 		self.goto(new_x,new_y)
+def make_blocks():
+	for i in range(blocks_num):
+		global me,me2
+		x= random.choice(xposes)
+		#block 6
+		if me==-2:
+			x1=x
+			me=-3
+		#block 5
+		if me==0:
+			x1=x
+			me=-1
+		#block 4
+		if me==2:
+			x1=x
+			me=1
+		#block 3
+		if me==4:
+			x1=x
+			me=3
+		#block 2
+		if me==6:
+			x1=x
+			me=5
+		#block1
+		if me==8:
+			x1=x
+			me=7
+		if me2==0:
+			global x1
+			while x1==x:
+				x= random.choice(xposes)
+				me-=1
 
-for i in range(blocks_num):
-	global me
-	if me==6:
-		x=xposes[0]
-		me=5
-	if me==5:
-		x=xposes[1]	
-		me=4
-	if me==4:
-		x=xposes[2]
-		me=3
-	if me==3:
-		x=xposes[3]
-		me=2
-	if me==2:
-		x=xposes[4]
-		me=2
-	if me==2:
-		x=xposes[5]
-		me=1
-	if me==1:
-		x=xposes[6]
-		me=0	
-
-	y=300
-	number=random.randint(1,20)
-	block=BLOCK(x,y,number)
-	block_list.append(block)
+		y=300
+		number=random.randint(1,20)
+		block=BLOCK(x,y,number)
+		block_list.append(block)
+		me2=0
 
 def move_blocks():
 
 	for i in block_list:
-		i.move(400,400)
-while True:
-	move_blocks()
+		i.move()
 
+
+
+
+while RUNNING==True:
+	stop=True
+	c=0
+	make_blocks()
+	while stop==True:
+		move_blocks()
+		c+=1
+		if c==10:
+			stop=False
+		getscreen().update()
+		time.sleep(SLEEP)
 
 
 mainloop()
